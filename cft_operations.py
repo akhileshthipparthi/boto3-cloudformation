c65ed3eb7ead1285ad29cc15c766d2469900954b
akhileshthipparthi <14009720+akhileshthipparthi@users.noreply.github.com>
2019-04-29 23:35:46 -0500

    Update cft_operations.py

--- cft_operations.py
+++ cft_operations.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
-from botocore.exceptions import ClientError
 import argparse
 import json
+import logging
 import sys
-import os
-import boto3
-from retrying import retry
-from dateutil.tz import tzutc
-import datetime
 import warnings
+
+import boto3
+from botocore.exceptions import ClientError
+from retrying import retry
+
 warnings.filterwarnings("ignore")
 
-transition_states = [
-    'CREATE_IN_PROGRESS', 'DELETE_IN_PROGRESS',
-    'ROLLBACK_IN_PROGRESS',
-    'UPDATE_IN_PROGRESS',
-    'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS',
-    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS',
-    'UPDATE_ROLLBACK_IN_PROGRESS',
-    'REVIEW_IN_PROGRESS']
+cfn_states = {
+    "transition_states": [
+        'CREATE_IN_PROGRESS', 'DELETE_IN_PROGRESS', 'ROLLBACK_IN_PROGRESS',
+        'UPDATE_IN_PROGRESS', 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS',
+        'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS', 'UPDATE_ROLLBACK_IN_PROGRESS', 'REVIEW_IN_PROGRESS'],
+    "update_states": ['CREATE_COMPLETE', 'UPDATE_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE'],
+    "delete_states": ['ROLLBACK_COMPLETE', 'ROLLBACK_FAILED', 'DELETE_FAILED', 'DELETE_COMPLETE']
+}
 
-update_states = ['CREATE_COMPLETE',
-                 'UPDATE_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE']
-delete_states = ['ROLLBACK_COMPLETE', 'ROLLBACK_FAILED',
-                 'DELETE_FAILED', 'DELETE_COMPLETE']
-
-# boto3.set_stream_logger('botocore', level=boto3.logging.DEBUG)
+logging.basicConfig(
+    format='%(levelname) -5s %(asctime)s %(funcName)- -20s: %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+    level=logging.INFO)
 
 
 def wait_for_complete(cft_client, cft_stack_name):
@@ -36,9 +34,10 @@ def wait_for_complete(cft_client, cft_stack_name):
         retry_on_exception=lambda ex: False)
     def wait_loop():
         stack_status = get_stack_status(cft_client, cft_stack_name)
-        if stack_status not in transition_states:
+        if stack_status not in cfn_states['transition_states']:
             return stack_status
-        print(f'StackStatus of {cft_stack_name} : {stack_status}.Waiting till State changes ..... ')
+        logging.info(
+            f'StackStatus of {cft_stack_name} : {stack_status}.Waiting till State changes ....... ')
 
     status = wait_loop()
 
@@ -47,6 +46,7 @@ def wait_for_complete(cft_client, cft_stack_name):
 
 def get_stack_status(cft_client, cft_stack_name):
 
+    logging.info(f'Get current stack status for : {cft_stack_name}')
     paginator = cft_client.get_paginator('describe_stacks')
 
     stack_iterator = paginator.paginate()
@@ -58,10 +58,31 @@ def get_stack_status(cft_client, cft_stack_name):
     return False
 
 
-def role_arn_to_session(role_arn, session_name):
+def cft_validate_stack(cft_client, cft_stack_name, cft_template_url):
 
-    lab = boto3.Session(profile_name='default')
-    # role_arn = "arn:aws:iam::" + account_id + ":role/" + role_name
+    logging.info(
+        f'Validating Stack {cft_stack_name} template for any Validation errors')
+
+    try:
+        response = cft_client.validate_template(
+            TemplateURL=cft_template_url
+        )
+    except Exception as e:
+        logging.exception(
+            f'ERROR: Validation error occurred in template and exception logged as :: {e}')
+        raise
+
+
+def role_arn_to_session(role_arn, session_name):
+    """
+        Assumes a role and returns boto session 
+
+    :param role_arn: role to assume
+    :param session_name: session name 
+    :return: boto3 Session object
+    """
+
+    lab = boto3.Session()
     client = lab.client('sts', verify=False)
     response = client.assume_role(
         RoleArn=role_arn,
@@ -73,75 +94,15 @@ def role_arn_to_session(role_arn, session_name):
         aws_session_token=response['Credentials']['SessionToken'])
 
 
-def cft_create_stack(role, parameter_data, cft_params):
-
-    cft_stack_name = parameter_data['StackName']
-    cft_client = role_arn_to_session(role, 'cftsession').client(
-        'cloudformation', region_name=parameter_data['RegionId'], verify=False)
-
-    current_stack_status = get_stack_status(cft_client, cft_stack_name)
-
-    # Validate the template before creating stacks.
-    cft_validate_stack(cft_client, cft_stack_name, parameter_data, cft_params)
-
-    if current_stack_status:
-        if current_stack_status not in transition_states and current_stack_status in update_states:
-            print(
-                f"{cft_stack_name} will be UPDATED due to its current statue -->  {current_stack_status}")
-
-            cft_update_stack(cft_client, cft_stack_name, parameter_data, cft_params)
-
-        else:
-            print(f"{cft_stack_name} Stack exists and couldnt be UPDATED due to its current transition/failed state --> {current_stack_status}.")
-
-        if current_stack_status in delete_states:
-            print(f"{cft_stack_name} will be DELETED due to its current state")
-
-            cft_delete_stack(cft_client, cft_stack_name, parameter_data, cft_params)
-
-    else:
-        print(f"{cft_stack_name} being CREATED")
-        try:
-            response = cft_client.create_stack(
-                StackName=cft_stack_name,
-                TemplateURL=parameter_data['TemplateUrl'],
-                Parameters=cft_params,
-                OnFailure='ROLLBACK',
-                Capabilities=['CAPABILITY_IAM'],
-                Tags=list(parameter_data.get('Tags', ''))
-            )
-
-            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-
-                stack_completion_status = wait_for_complete(
-                    cft_client, cft_stack_name)
-
-                if stack_completion_status == 'CREATE_COMPLETE':
-                    print(
-                        f'{cft_stack_name} Stack CREATED successfully.Current State {stack_completion_status}')
-                else:
-                    for event in cft_client.describe_stack_events(StackName=cft_stack_name)['StackEvents']:
-                        del event['Timestamp']
-                        print(f'{json.dumps(event,indent=2)}')
-                    raise Exception(
-                        f"ERROR: {cft_stack_name} Stack Couldn't be CREATED.Current State {stack_completion_status}.Stack Events logged")
-            else:
-                print(
-                    f"{cft_stack_name} Stack Couldnt be CREATED.Error Calling API {json.dumps(response)}")
-
-        except Exception as e:
-            raise Exception("ERROR: Exception CREATING Stack", e)
-
-
-def cft_update_stack(cft_client, stack_name, parameter_data, cft_params):
+def cft_update_stack(cft_client, stack_name, cft_template_url, cft_params, cft_tags):
 
     try:
         response = cft_client.update_stack(
             StackName=stack_name,
-            TemplateURL=parameter_data['TemplateUrl'],
+            TemplateURL=cft_template_url,
             Parameters=cft_params,
-            Capabilities=['CAPABILITY_IAM'],
-            Tags=list(parameter_data.get('Tags', '')))
+            Capabilities=["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"],
+            Tags=cft_tags)
 
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
 
@@ -149,30 +110,35 @@ def cft_update_stack(cft_client, stack_name, parameter_data, cft_params):
 
             if stack_completion_status == 'UPDATE_COMPLETE':
 
-                print(
+                logging.info(
                     f'{stack_name} Stack UPDATED successfully.Current State {stack_completion_status}')
             else:
-                for event in cft_client.describe_stack_events(StackName=stack_name)['StackEvents']:
+                for event in cft_client.describe_stack_events(
+                        StackName=stack_name)['StackEvents']:
                     del event['Timestamp']
                     print(f'{json.dumps(event,indent=2)}')
-                raise Exception(
+                logging.error(
                     f"ERROR: {stack_name} Stack Couldn't be UPDATED.Current State {stack_completion_status}.Stack Events logged")
-
+                sys.exit(1)
         else:
-            print(
+            logging.error(
                 f"ERROR: {stack_name} Stack Couldnt be UPDATED.Error Calling API {json.dumps(response)}")
+            sys.exit(1)
 
     except ClientError as e:
         if e.response['Error']['Message'] == 'No updates are to be performed.':
-            print(f'Stack {stack_name}  Already Updated.Nothing to update')
+            logging.info(
+                f'Stack {stack_name} Already Updated.Nothing to update')
         else:
-            raise Exception(
-                f"ERROR: Exception UPDATING Stack {stack_name} {e}")
+            logging.error(f"ERROR: Exception UPDATING Stack {stack_name} {e}")
+            sys.exit(1)
     except Exception as e:
-        raise Exception(f"ERROR: Exception UPDATING Stack {stack_name} :: {e}")
+        logging.exception(
+            f"ERROR: Exception UPDATING Stack {stack_name} :: {e}")
+        raise
 
 
-def cft_delete_stack(cft_client, stack_name, parameter_data, cft_params):
+def cft_delete_stack(cft_client, stack_name):
 
     try:
         response = cft_client.delete_stack(StackName=stack_name)
@@ -182,73 +148,157 @@ def cft_delete_stack(cft_client, stack_name, parameter_data, cft_params):
             stack_completion_status = wait_for_complete(cft_client, stack_name)
 
             if not stack_completion_status:
-                print(f'{stack_name} Stack DELETED successfully')
+                logging.info(f'{stack_name} Stack DELETED successfully')
             else:
-                raise Exception(
+                logging.error(
                     f"ERROR: {stack_name} Stack Couldn't be DELETED.Check Stack Events.")
+                sys.exit(1)
 
     except Exception as e:
-        raise Exception(f"ERROR: {stack_name} Stack Couldn't be DELETED.")
+        logging.exception(
+            f"ERROR: Exception DELETING Stack {stack_name} :: {e}")
+        raise
 
 
-def cft_validate_stack(cft_client, cft_stack_name, parameter_data, cft_params):
+def cft_create_stack(parameter_data, cft_params, cft_tags, cft_template_url, cft_role):
 
-    print(
-        f'Validating Stack {cft_stack_name} template for any Validation errors')
-    try:
-        response = cft_client.validate_template(
-            TemplateURL=parameter_data['TemplateUrl']
-        )
-    except ClientError as e:
-        raise Exception(
-            f'ERROR: Validation error occurred in template and exception logged as : ', e)
+    cft_stack_name = parameter_data['StackName']
+    cft_client = role_arn_to_session(cft_role, 'cftsession').client(
+        'cloudformation', region_name=parameter_data['RegionId'], verify=False)
 
+    current_stack_status = get_stack_status(cft_client, cft_stack_name)
 
-def parse_parameters(param_file):
+    # Validate the template before creating stacks.
+    cft_validate_stack(cft_client, cft_stack_name, cft_template_url)
 
-    try:
-        with open(param_file) as f:
-            parameter_data = json.load(f)
-            cft_params = []
-        mandatory_params = ['RegionId', 'TemplateUrl', 'StackName']
-        if all(k in parameter_data for k in mandatory_params):
-            for k in parameter_data.keys():
-                if k not in mandatory_params and k != 'Tags':
-                    cft_params.append(
-                        {"ParameterKey": k, "ParameterValue": parameter_data[k]})
-            return parameter_data, cft_params
+    if current_stack_status:
+
+        logging.info(f"Stack :: {cft_stack_name} already exists")
+        if current_stack_status not in cfn_states['transition_states'] and current_stack_status in cfn_states['update_states']:
+            logging.info(
+                f"Stack :: {cft_stack_name} will be UPDATED due to its current statue -->  {current_stack_status}")
+
+            cft_update_stack(cft_client, cft_stack_name,
+                             cft_template_url, cft_params, cft_tags)
+
         else:
-            sys.exit(
+            logging.info(
+                f"Stack :: {cft_stack_name} exists and couldnt be UPDATED due to its current transition/failed state --> {current_stack_status}.")
+
+            if current_stack_status in cfn_states['transition_states']:
+                    stack_completion_status = wait_for_complete(cft_client, stack_name)
+
+        if current_stack_status in cfn_states['delete_states']:
+
+            logging.info(
+                f"{cft_stack_name} will be DELETED due to its current state")
+
+            cft_delete_stack(cft_client, cft_stack_name)
+
+    else:
+        logging.info(f"{cft_stack_name} being CREATED")
+        try:
+            response = cft_client.create_stack(
+                StackName=cft_stack_name,
+                TemplateURL=cft_template_url,
+                Parameters=cft_params,
+                OnFailure='ROLLBACK',
+                Capabilities=['CAPABILITY_IAM'],
+                Tags=cft_tags)
+
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+
+                stack_completion_status = wait_for_complete(
+                    cft_client, cft_stack_name)
+
+                if stack_completion_status == 'CREATE_COMPLETE':
+                    logging.info(
+                        f'{cft_stack_name} Stack CREATED successfully.Current State {stack_completion_status}')
+                else:
+                    for event in cft_client.describe_stack_events(
+                            StackName=cft_stack_name)['StackEvents']:
+                        del event['Timestamp']
+                        print(f'{json.dumps(event,indent=2)}')
+                    logging.error(
+                        f"ERROR: {cft_stack_name} Stack Couldn't be CREATED.Current State {stack_completion_status}.Stack Events logged")
+                    sys.exit(1)
+            else:
+                logging.error(
+                    f"{cft_stack_name} Stack Couldnt be CREATED.Error Calling API {json.dumps(response)}")
+                sys.exit(1)
+
+        except Exception as e:
+            logging.exception("ERROR: Exception CREATING Stack")
+            raise
+
+
+def parse_parameters(param_file, environment):
+    logging.info(f'Fetching Parameters from {param_file}')
+    try:
+        with open(param_file, mode='r') as f:
+            parameter_data = json.load(f)
+
+        mandatory_params = ['RegionId', 'StackName', 'Environment']
+
+        if all(k in parameter_data for k in mandatory_params):
+
+            # Specific parameters to environment
+            cft_params = list(parameter_data['Environment'].get(
+                environment).get('Parameters', ''))
+            # global parameters
+            if parameter_data.get('Parameters'):
+                if parameter_data['Parameters'][0]['ParameterKey'] == "Environment":
+                    parameter_data['Parameters'][0]['ParameterValue'] = environment
+                    for i in parameter_data.get('Parameters'):
+                        cft_params.append(i)
+            # Tags if present
+            cft_tags = list(parameter_data.get('Tags', ''))
+            cft_template_url = parameter_data['Environment'].get(environment, '')[
+                'TemplateUrl']
+            cft_role = parameter_data['Environment'].get(environment, '')[
+                'Role_Arn']
+            return parameter_data, cft_params, cft_tags, cft_template_url, cft_role
+        else:
+            logging.error(
                 f'Mandatory parameters:{mandatory_params} are missing in Parameters file.Exiting ')
+            sys.exit(1)
     except IOError:
-        print("Error: Cant open or read file")
+        logging.error(
+            f'File {param_file} not found,or unable to open,Exiting')
+        sys.exit(1)
     except Exception as e:
-        raise Exception('ERROR : Issue loading Paramater file ', e)
+        logging.exception(f'ERROR:: Failed with exception :: {e}')
+        raise
 
 
 def main():
-
     parser = argparse.ArgumentParser(
         description='Deployment script parameters')
 
     parser.add_argument(
         '-p',
         '--params',
-        dest='param_file',
+        dest='parameter_file',
         required=True,
-        help="Parmater template file for CFT")
+        help="Parmater template file for CFT , in json format")
     parser.add_argument(
-        '-r',
-        '--role',
-        dest='role',
+        '-e',
+        '--environment',
+        dest='environment',
         required=True,
-        help="Role to Assume")
+        help="Environment")
 
     args = parser.parse_args()
 
-    parameter_data, cft_params = parse_parameters(args.param_file)
+    logging.info('Starting Deployment')
 
-    cft_create_stack(args.role, parameter_data, cft_params)
+    parameter_data, cft_params, cft_tags, cft_template_url, cft_role = parse_parameters(
+        args.parameter_file, args.environment)
+
+    cft_create_stack(parameter_data, cft_params,
+                     cft_tags, cft_template_url, cft_role)
+
+    logging.info('Deployment completed')
 
 
 if __name__ == '__main__':
